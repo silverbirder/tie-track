a4ffee852cfae1e6fc69d40ff43f5c440b2ea9da
shibamoto <silverbirder@gmail.com>
2024-09-16 21:57:18 +0900

    first commit

---
+++ drizzle/0000_free_thor.sql
@@ -0,0 +1,66 @@
+CREATE TABLE IF NOT EXISTS "tie-track_account" (
+	"user_id" varchar(255) NOT NULL,
+	"type" varchar(255) NOT NULL,
+	"provider" varchar(255) NOT NULL,
+	"provider_account_id" varchar(255) NOT NULL,
+	"refresh_token" text,
+	"access_token" text,
+	"expires_at" integer,
+	"token_type" varchar(255),
+	"scope" varchar(255),
+	"id_token" text,
+	"session_state" varchar(255),
+	CONSTRAINT "tie-track_account_provider_provider_account_id_pk" PRIMARY KEY("provider","provider_account_id")
+);
+--> statement-breakpoint
+CREATE TABLE IF NOT EXISTS "tie-track_post" (
+	"id" serial PRIMARY KEY NOT NULL,
+	"name" varchar(256),
+	"created_by" varchar(255) NOT NULL,
+	"created_at" timestamp with time zone DEFAULT CURRENT_TIMESTAMP NOT NULL,
+	"updated_at" timestamp with time zone
+);
+--> statement-breakpoint
+CREATE TABLE IF NOT EXISTS "tie-track_session" (
+	"session_token" varchar(255) PRIMARY KEY NOT NULL,
+	"user_id" varchar(255) NOT NULL,
+	"expires" timestamp with time zone NOT NULL
+);
+--> statement-breakpoint
+CREATE TABLE IF NOT EXISTS "tie-track_user" (
+	"id" varchar(255) PRIMARY KEY NOT NULL,
+	"name" varchar(255),
+	"email" varchar(255) NOT NULL,
+	"email_verified" timestamp with time zone DEFAULT CURRENT_TIMESTAMP,
+	"image" varchar(255)
+);
+--> statement-breakpoint
+CREATE TABLE IF NOT EXISTS "tie-track_verification_token" (
+	"identifier" varchar(255) NOT NULL,
+	"token" varchar(255) NOT NULL,
+	"expires" timestamp with time zone NOT NULL,
+	CONSTRAINT "tie-track_verification_token_identifier_token_pk" PRIMARY KEY("identifier","token")
+);
+--> statement-breakpoint
+DO $$ BEGIN
+ ALTER TABLE "tie-track_account" ADD CONSTRAINT "tie-track_account_user_id_tie-track_user_id_fk" FOREIGN KEY ("user_id") REFERENCES "public"."tie-track_user"("id") ON DELETE no action ON UPDATE no action;
+EXCEPTION
+ WHEN duplicate_object THEN null;
+END $$;
+--> statement-breakpoint
+DO $$ BEGIN
+ ALTER TABLE "tie-track_post" ADD CONSTRAINT "tie-track_post_created_by_tie-track_user_id_fk" FOREIGN KEY ("created_by") REFERENCES "public"."tie-track_user"("id") ON DELETE no action ON UPDATE no action;
+EXCEPTION
+ WHEN duplicate_object THEN null;
+END $$;
+--> statement-breakpoint
+DO $$ BEGIN
+ ALTER TABLE "tie-track_session" ADD CONSTRAINT "tie-track_session_user_id_tie-track_user_id_fk" FOREIGN KEY ("user_id") REFERENCES "public"."tie-track_user"("id") ON DELETE no action ON UPDATE no action;
+EXCEPTION
+ WHEN duplicate_object THEN null;
+END $$;
+--> statement-breakpoint
+CREATE INDEX IF NOT EXISTS "account_user_id_idx" ON "tie-track_account" USING btree ("user_id");--> statement-breakpoint
+CREATE INDEX IF NOT EXISTS "created_by_idx" ON "tie-track_post" USING btree ("created_by");--> statement-breakpoint
+CREATE INDEX IF NOT EXISTS "name_idx" ON "tie-track_post" USING btree ("name");--> statement-breakpoint
+CREATE INDEX IF NOT EXISTS "session_user_id_idx" ON "tie-track_session" USING btree ("user_id");
